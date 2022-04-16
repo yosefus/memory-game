7e1810f5fa640b1468534e80c52a2c0c73ff471f
yosefus <yosalsoni@gmail.com>
2022-04-16 23:09:30 +0300

    add reset add icons add num of cards- fixed cards animation

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let root = document.querySelector("#root"),
    note = document.querySelector("#note"),
    p1 = document.querySelector("#player1Box"),
    p2 = document.querySelector("#player2Box"),
-   boardArr = ["a", "b", "c", "d", "e"],
-   currBoardArr = boardArr.concat(boardArr),
-   Player1 = { name: "", score: 0 },
-   Player2 = { name: "", score: 0 },
+   btnBox = document.getElementById("btn-box"),
+   boardArr = ["🐵", "🐶", "🐺", "🐱", "🦁", "🐯", "🦒", "🦊", "🐻", "🐰", "🐹", "🐭", "🐗", "🐷", "🐮", "🦝", "🐨", "🐼", "🐸", "🦓", "🐴", "🦄", "🐔", "🐲"],
+   cardsNum = boardArr.length - 1,
+   currBoardArr = boardArr.slice(0, cardsNum).concat(boardArr.slice(0, cardsNum)),
+   Player1 = { name: "", score: 0, totalScore: 0 },
+   Player2 = { name: "", score: 0, totalScore: 0 },
    currPlay = [],
    middlePlay = false,
    currPlayer = Player1,
@@ -19,7 +21,8 @@ function onClickCard(e) {
 
    currPlay.push(pos)
    deleteAnimate(e.target)
-   e.target.className += " animate__flip"
+   e.target.offsetWidth
+   e.target.classList.add("animate__flip")
    e.target.innerHTML = currBoardArr[pos]
 
    if (currPlay.length == 2) {
@@ -48,21 +51,22 @@ function reset() {
 }
 
 function printWin() {
-   let winner
+   let winner = {}
 
    if (Player1.score === Player2.score) {
       winner.name = "no one"
    } else {
       winner = Player1.score > Player2.score ? Player1 : Player2
+      winner.totalScore++
    }
 
-   note.className = "popUp"
+   note.classList.add("popUp")
    note.innerHTML = winner.name + ` is the winner`
    setTimeout(reset, 4000);
 }
 
 function deleteAnimate(element) {
-   [...rollAnimateArr, "animate__flip"].forEach(st => element.classList.remove(st))
+   element.classList.remove(...rollAnimateArr, "animate__flip")
    return element
 }
 
@@ -70,8 +74,8 @@ function checkWins() {
    if (currBoardArr[currPlay[0]] == currBoardArr[currPlay[1]]) {
       Array.from(Array(2)).forEach((x, i) => {
          let card = document.getElementById(`b${currPlay[i]}`)
-         deleteAnimate(card).className += " animate__heartBeat bg-warning"
-         setTimeout(() => card.className += " gone", 3000);
+         deleteAnimate(card).classList.add("animate__heartBeat", "bg-warning")
+         setTimeout(() => card.classList.add("gone"), 3000);
 
          currBoardArr = currBoardArr.map((c) => c == currBoardArr[currPlay[i]] ? "?" : c)
       })
@@ -94,10 +98,10 @@ function print(first) {
 
    currBoardArr.forEach((x, i) => {
       const contain = document.createElement("div")
-      contain.className = `p-2 p-md-4 col-3`
+      contain.classList.add("p-2", `${currBoardArr.length <= 16 ? "p-md-4" : "p-md-2"}`, `${currBoardArr.length <= 16 ? "col-3" : "col-2"}`)
 
       const div = document.createElement("div")
-      div.className = `card d-flex justify-content-center align-items-center bg-success animate__animated ${first ? rollAnimateArr[Math.floor(Math.random() * rollAnimateArr.length)] : ""}`
+      div.classList.add("card", "d-flex", "justify-content-center", "align-items-center", "bg-success", "animate__animated", `${first ? rollAnimateArr[Math.floor(Math.random() * rollAnimateArr.length)] : ""}`)
       div.id = `b${i}`
       div.addEventListener("click", onClickCard)
 
@@ -114,20 +118,36 @@ function printPlayers() {
       const h2 = document.createElement("h2")
       h2.innerText = prop[`Player${i + 1}`].name
       const p = document.createElement("p")
-      p.innerText = prop[`Player${i + 1}`].score
-      prop[`p${i + 1}`].append(h2, p)
+      p.innerText = "score: " + prop[`Player${i + 1}`].score
+      const p2 = document.createElement("p")
+      p2.innerText = "total score: " + prop[`Player${i + 1}`].totalScore
+      prop[`p${i + 1}`].append(h2, p, p2)
    })
 }
 
 function firstPrint() {
    currBoardArr.sort(() => Math.random() - 0.5)
+   addButtons()
    print(true)
+}
+
+function addButtons() {
+   const btn = document.createElement("button")
+   btn.innerText = "reset"
+   btn.classList.add("btn", "btn-danger")
+   btn.addEventListener("click", firstPrint)
+   btnBox.innerHTML = ""
+   btnBox.appendChild(btn)
 }
 
 function submitNames(e) {
    e.preventDefault()
    Player1.name = document.getElementById(`input0`).value
    Player2.name = document.getElementById(`input1`).value
+
+   cardsNum = Number(document.getElementById(`numBoard`).value) || cardsNum
+   currBoardArr = boardArr.slice(0, cardsNum).concat(boardArr.slice(0, cardsNum))
+
    firstPrint()
 }
 
@@ -149,12 +169,23 @@ function formPrint() {
       return inputName
    })
 
+   let numInput = document.createElement("input")
+   numInput.className = "form-control"
+   numInput.type = "number"
+   numInput.min = 2
+   numInput.max = boardArr.length
+   numInput.id = "numBoard"
+
+   let label = document.createElement("label")
+   label.for = "numBoard"
+   label.innerText = "number of cards"
+
    const button = document.createElement("button")
    button.type = "submit"
    button.innerText = "start the game"
    button.className = "btn btn-success w-50 w-md-25 m-auto"
 
-   form.append(h1, ...inputs, button)
+   form.append(h1, ...inputs, label, numInput, button)
    root.append(form)
 }
 
